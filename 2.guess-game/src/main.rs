24d1f9f18ee1d4d979be8e101954281246b8b9b8
Bayu Kurnia <hi@bayukurnia.com>
2022-04-08 03:07:50 +0700

    compare guess with secret_number

--- 2.guess-game/src/main.rs
+++ 2.guess-game/src/main.rs
@@ -1,12 +1,11 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..101);
-
-    println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess.");
 
@@ -16,5 +15,17 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    // Rust allow shadowing of variables. wow!,
+    // for make `cmp` to work, we need to use same type of variable
+    // this infer guess to u32 (same as secret_number)
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {}", guess);
+
+    // Compare guess to secret number
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    };
 }
