32ea2d4b2bb7c73d190dd44b222fc423aae865ba
Bayu Kurnia <hi@bayukurnia.com>
2022-04-08 17:57:12 +0700

    const variable

--- 3.1.variables/src/main.rs
+++ 3.1.variables/src/main.rs
@@ -5,4 +5,9 @@ fn main() {
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+
+    // const
+    // always immutable, can't use `mut` keyword
+    const THREE_HOURS_IN_SECONDS: i32 = 60 * 60 * 3;
+    println!("Three hours in seconds: {}", THREE_HOURS_IN_SECONDS);
 }
