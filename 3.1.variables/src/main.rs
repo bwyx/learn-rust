46b58ec7cbb5b727af9da209417312d45cddcbd2
Bayu Kurnia <hi@bayukurnia.com>
2022-04-08 17:50:04 +0700

    let variable

---
+++ 3.1.variables/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // let
+    // able to make variable mutable with `mut`
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+}
